f301f13c0f198f01928b45a6ebe186799a159f3f
Anna-Anka <anjbekrenewa@gmail.com>
2022-10-21 18:17:26 +0300

    corrected readme

--- gulpfile.js
+++ gulpfile.js
@@ -92,7 +92,6 @@ function scripts() {
             'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
             'node_modules/air-datepicker/dist/js/datepicker.min.js',
             'node_modules/rateyo/src/jquery.rateyo.js',
-            'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
             'node_modules/swiper/swiper-bundle.min.js',
             'app/js/main.js'
         ])
